df4cded68c7410e2e57fb5c626e7326fc1d28e39
Wes Yarber <78522869+WesYarber@users.noreply.github.com>
2021-02-04 16:17:15 -0600

    Update t2_rag2.c

--- Lab2/source_files/t2_rag2.c
+++ Lab2/source_files/t2_rag2.c
@@ -24,30 +24,30 @@ ESOS_USER_TASK(state_machine) {
         state[NORTH_SOUTH] = RED;
         state[EAST_WEST] = GREEN;
         
-        if(SW1_PRESSED) ESOS_TASK_WAIT_TICKS(30000); // Wait for 30s
-        else ESOS_TASK_WAIT_TICKS(30000); // Wait for 10s
+        if(SW1_PRESSED) ESOS_TASK_WAIT_TICKS(30000) // Wait for 30s
+        else ESOS_TASK_WAIT_TICKS(10000) // Wait for 10s
         
         state[EAST_WEST] = AMBER;
         
-        ESOS_TASK_WAIT_TICKS(3000); // Wait for 3s
+        ESOS_TASK_WAIT_TICKS(3000) // Wait for 3s
         
         if(SW1_PRESSED) {
             state[EAST_WEST] = RED;
-            ESOS_TASK_WAIT_TICKS(1000);
+            ESOS_TASK_WAIT_TICKS(1000) // Wait for 1s
         }
         
         state[NORTH_SOUTH] = GREEN;
         state[EAST_WEST] = RED;
         
-        if(SW1_PRESSED) ESOS_TASK_WAIT_TICKS(30000); // Wait for 30s
-        else ESOS_TASK_WAIT_TICKS(30000); // Wait for 10s
+        if(SW1_PRESSED) ESOS_TASK_WAIT_TICKS(30000) // Wait for 30s
+        else ESOS_TASK_WAIT_TICKS(10000) // Wait for 10s
         
         state[NORTH_SOUTH] = AMBER;
-        ESOS_TASK_WAIT_TICKS(3000); // Wait for 3s
+        ESOS_TASK_WAIT_TICKS(3000) // Wait for 3s
         
         if(SW1_PRESSED) {
-            state[NORTH_SOUTH] = RED;
-            ESOS_TASK_WAIT_TICKS(1000);
+            state[EAST_WEST] = RED;
+            ESOS_TASK_WAIT_TICKS(1000) // Wait for 1s
         }
     }
     ESOS_TASK_END();
@@ -87,6 +87,7 @@ void user_init()
     CONFIG_LED1();
     CONFIG_LED2();
     CONFIG_LED3();
+    CONFIG_SW1();
     CONFIG_SW3();
     
     esos_RegisterTask(state_machine);
