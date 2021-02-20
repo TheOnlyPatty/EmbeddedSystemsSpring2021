8207afbf7bc4bc1a1dd16109fe3f6808b48b71e8
Joshua McCown <jpm623@msstate.edu>
2021-02-20 17:43:10 -0600

    t3_app.c rpg completely done, just have the LED/SW stuff left

--- Lab3/build/source_files/t3_app.c
+++ Lab3/build/source_files/t3_app.c
@@ -3,27 +3,162 @@
 #include "esos_f14ui.h"
 #include "t3_interface.h"
 
+//strings for printing state changes
+
+static char str_RPG_CW_FAST[] = "RPG turning CW fast\n";
+static char str_RPG_CW_MED[] = "RPG turning CW medium\n";
+static char str_RPG_CW_SLOW[] = "RPG turning CW slow\n";
+static char str_RPG_CCW_FAST[] = "RPG turning CCW fast\n";
+static char str_RPG_CCW_MED[] = "RPG turning CCW medium\n";
+static char str_RPG_CCW_SLOW[] = "RPG turning CCW slow\n";
+static char str_RPG_NO_MOVEMENT[] = "RPG not Turning\n";
+static char str_SW1_P[] = "SW1 Pressed\n";
+static char str_SW1_DP[] = "SW1 Double Pressed\n";
+static char str_SW2_P[] = "SW2 Pressed\n";
+static char str_SW2_DP[] = "SW1 Double Pressed\n";
+static char str_SW3_P[] = "SW3 Pressed\n";
+static char str_SW3_DP[] = "SW3 Double Pressed\n";
+
+
 ESOS_USER_TASK(rpg) {
     ESOS_TASK_BEGIN();
+	
+	BOOL IF_CW_FAST = FALSE;
+	BOOL IF_CCW_FAST = FALSE;
+	BOOL IF_CW_MED = FALSE;
+	BOOL IF_CCW_MED = FALSE;
+	BOOL IF_CW_SLOW = FALSE;
+	BOOL IF_CCW_SLOW = FALSE;
+	BOOL IF_NO_MOVE = FALSE;
+
     while(TRUE) {
+		
+		
+		
         if (esos_uiF14_isRPGTurningFast() == TRUE) {
+			
             esos_uiF14_flashLED2(100);
+			
+			if (esos_uiF14_isRPGTurningCW() && !IF_CW_FAST) {
+				
+				IF_CW_FAST = TRUE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CW_FAST);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
+			else if (esos_uiF14_isRPGTurningCCW() && !IF_CCW_FAST){
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = TRUE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CCW_FAST);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
+			
         }
         else if (esos_uiF14_isRPGTurningMedium() == TRUE) {
+			
             esos_uiF14_flashLED2(500);
+			
+			if (esos_uiF14_isRPGTurningCW() && !IF_CW_MED) {
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = TRUE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CW_MED);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
+			else if (esos_uiF14_isRPGTurningCCW() && !IF_CCW_MED){
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = TRUE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CCW_MED);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
         }
         else if (esos_uiF14_isRPGTurningSlow() == TRUE) {
             esos_uiF14_turnLED2On();
+			
+			if (esos_uiF14_isRPGTurningCW() && !IF_CW_SLOW) {
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = TRUE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CW_SLOW);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
+			else if (esos_uiF14_isRPGTurningCCW() && !IF_CCW_SLOW){
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = TRUE;
+				IF_NO_MOVE = FALSE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_CCW_SLOW);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+			}
+			
         }
         else if (esos_uiF14_isRPGTurning() == FALSE) {
             esos_uiF14_turnLED2Off();
-        }
-
-        if (esos_uiF14_isRPGTurningCW() == TRUE) {
-            printf("\n\nCLOCKWISE\n\n");
-        }
-        if (esos_uiF14_isRPGTurningCCW() == TRUE) {
-            printf("\n\nCOUNTER-CLOCKWISE\n\n");
+			
+			if(!IF_NO_MOVE){
+				
+				IF_CW_FAST = FALSE;
+				IF_CCW_FAST = FALSE;
+				IF_CW_MED = FALSE;
+				IF_CCW_MED = FALSE;
+				IF_CW_SLOW = FALSE;
+				IF_CCW_SLOW = FALSE;
+				IF_NO_MOVE = TRUE;
+				
+				ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+				ESOS_TASK_WAIT_ON_SEND_STRING(str_RPG_NO_MOVEMENT);
+				ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+				
+			}
+			
         }
 
         ESOS_TASK_YIELD();
@@ -69,4 +204,5 @@ void user_init(void) {
 
     esos_RegisterTask(rpg);
     esos_RegisterTask(LED);
+
 }
---
+++ Lab3/esos/include/t3_interface.h
@@ -0,0 +1,5 @@
+#pragma once 
+
+#include "esos.h"
+
+ESOS_USER_TASK(menu);
