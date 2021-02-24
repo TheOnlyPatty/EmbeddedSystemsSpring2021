c1f27d20c57a548f099e7ff0ef988f572bf0e48b
WesYarber <austiny@mac.com>
2021-02-24 17:54:22 -0600

    added hw back

--- Lab4/esos/src/esos_pic24_sensor.c
+++ Lab4/esos/src/esos_pic24_sensor.c
@@ -4,31 +4,26 @@
 //Configure and enable the sensor module for hwxxx hardware.
 // e_senCh   specifies sensor channel
 // e_senVRef specifies sensor voltage reference
-void esos_sensor_config (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef)
-{
-
+void esos_sensor_config_hw (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef) {
+    
 }
 
 //Determine if the sensor is currently converting
-BOOL esos_sensor_is_converting (void)
-{
-
+BOOL esos_sensor_is_converting_hw (void) {
+    
 }
 
 //Initiate a conversion for a configured sensor
-void esos_sensor_initiate_conversion (void)
-{
-
+void esos_sensor_initiate_conversion_hw (void) {
+    
 }
 
 //Receive the value from a conversion that has already been initiated
-uint16_t esos_sensor_getvalue_u16 (void)
-{
-
+uint16_t esos_sensor_getvalue_u16_hw (void) {
+    
 }
 
 //Release any pending conversions for the sensor
-void esos_sensor_release (void)
-{
-
+void esos_sensor_release_hw (void) {
+    
 }
