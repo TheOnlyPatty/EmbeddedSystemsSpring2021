3133163a6106f4e100a9db3dfe42961cdabdcd12
WesYarber <austiny@mac.com>
2021-02-24 17:49:23 -0600

    ADC Config testing

--- Lab4/esos/include/esos_pic24_sensor.h
+++ Lab4/esos/include/esos_pic24_sensor.h
@@ -4,10 +4,10 @@
 #include <esos_sensor.h>
 #include "revF14.h"
 
-void esos_sensor_config_hw (esos_sensor_ch_t, esos_sensor_vref_t);
-void esos_sensor_initiate_conversion_hw (void);
-uint16_t esos_sensor_getvalue_u16_hw (void);
-void esos_sensor_release_hw (void);
-BOOL esos_sensor_is_converting_hw (void);
+void esos_sensor_config (esos_sensor_ch_t, esos_sensor_vref_t);
+void esos_sensor_initiate_conversion (void);
+uint16_t esos_sensor_getvalue_u16 (void);
+void esos_sensor_release (void);
+BOOL esos_sensor_is_converting (void);
 
 #endif
--- Lab4/esos/src/esos_pic24_sensor.c
+++ Lab4/esos/src/esos_pic24_sensor.c
@@ -4,31 +4,31 @@
 //Configure and enable the sensor module for hwxxx hardware.
 // e_senCh   specifies sensor channel
 // e_senVRef specifies sensor voltage reference
-void esos_sensor_config_hw (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef)
+void esos_sensor_config (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef)
 {
 
 }
 
-//Determine truth of: the sensor is currently converting
-BOOL esos_sensor_is_converting_hw (void)
+//Determine if the sensor is currently converting
+BOOL esos_sensor_is_converting (void)
 {
 
 }
 
 //Initiate a conversion for a configured sensor
-void esos_sensor_initiate_conversion_hw (void)
+void esos_sensor_initiate_conversion (void)
 {
 
 }
 
 //Receive the value from a conversion that has already been initiated
-uint16_t esos_sensor_getvalue_u16_hw (void)
+uint16_t esos_sensor_getvalue_u16 (void)
 {
 
 }
 
 //Release any pending conversions for the sensor
-void esos_sensor_release_hw (void)
+void esos_sensor_release (void)
 {
 
 }
