33918ccdc1ec11c064a4347b0723db0bd5961d6b
WesYarber <austiny@mac.com>
2021-02-24 19:45:48 -0600

    Futher progress on libraries and t4_sensor1.c

--- Lab4/esos/src/esos_pic24_sensor.c
+++ Lab4/esos/src/esos_pic24_sensor.c
@@ -5,7 +5,15 @@
 // e_senCh   specifies sensor channel
 // e_senVRef specifies sensor voltage reference
 void esos_sensor_config_hw (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef) {
-    
+    CONFIG_ADC();
+    AD1CON1bits.ADON = 0; //Turn ADC off
+    AD1CHS0bits.CH0SA = e_senCh; //Set CH0+ input AN[e_senCh]
+    AD1CHS0bits.CH0NA = 0; //Set CHO- input to VREFL (AVss)
+    AD1CHS123 = 0; //Disable CH's 1,2,&3
+    AD1CSSH = 0x0000; //No inputs are scanned
+    AD1CSSL = 0x0000; //Set input scan select register low
+//    IFS1CLR = 2; //Clear ADC conversion interrupt
+    AD1CON1bits.ADON = 1; //Turn ADC on
 }
 
 //Determine if the sensor is currently converting
@@ -16,6 +24,8 @@ BOOL esos_sensor_is_converting_hw (void) {
 //Initiate a conversion for a configured sensor
 void esos_sensor_initiate_conversion_hw (void) {
     
+    esos_SetUserFlag(__ESOS_SYS_ADC_IS_BUSY);
+    AD1CON1bits.SAMP = 1; //Take sample from ADC
 }
 
 //Receive the value from a conversion that has already been initiated
--- Lab4/source_files/t4_sensor.c
+++ Lab4/source_files/t4_sensor.c
@@ -4,11 +4,30 @@
 //#include "t3_interface.h"
 #include <stdio.h>
 
+static char str_INSTRUCTIONS1[] = "Press SW1 to sample the potentiometer once.\nPress SW2 to begin sampling the potentiometer once per second.\n";
+static char str_INSTRUCTIONS2[] = "Press SW1 or SW2 to quit sampling.\n"
+
+static uint16_t u16_data;
+
+
+ESOS_USER_TASK(SerialOutput) {
+    static uint16_t state = 0;
+    static uint16_t previous_state = 0;
+    ESOS_TASK_BEGIN();
+    while(1) {
+        ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(POT_CHANNEL,ESOS_SENSOR_VREF_3V3);
+        ESOS_TASK_WAIT_SENSOR_READ(u16_data,
+        ESOS_SENSOR_CLOSE();
+        
+        ESOS_TASK_YIELD();
+    }
+    ESOS_TASK_END();
+}
 
 void user_init(void) {
     config_esos_uiF14();
 //    config_ui_menu();
-
     esos_uiF14_flashLED3(500); //500 ms flash on LED3. (Heartbeat)
+    esos_RegisterTask(SerialOutput);
 
 }
