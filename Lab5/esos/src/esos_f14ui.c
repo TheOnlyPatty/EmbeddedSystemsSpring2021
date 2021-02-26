f776dffab71cea7569846026f1e201890acd82bd
WesYarber <austiny@mac.com>
2021-02-26 14:14:57 -0600

    Added Lab5 dir

---
+++ Lab5/esos/src/esos_f14ui.c
@@ -0,0 +1,477 @@
+/*******************************************************************
+ *
+ * C code framework for ESOS user-interface (UI) service
+ *
+ *    requires the EMBEDDED SYSTEMS target rev. F14
+ *
+ * ****************************************************************/
+
+#include "esos.h"
+#include "esos_pic24.h"
+#include "esos_f14ui.h"
+#include "revF14.h"
+#include <stdio.h>
+
+_st_esos_uiF14Data_t _st_esos_uiF14Data;
+
+    // PRIVATE FUNCTIONS
+inline void _esos_uiF14_setRPGCounter (uint16_t newValue) {
+    _st_esos_uiF14Data.i16_RPGCounter = newValue;
+    return;
+}
+inline void _esos_uiF14_setLastRPGCounter (uint16_t newValue) {
+    _st_esos_uiF14Data.i16_lastRPGCounter = newValue;
+    return;
+}
+
+    // PUBLIC SWITCH FUNCTIONS
+
+//SW1
+inline BOOL esos_uiF14_isSW1Pressed (void) {
+    return _st_esos_uiF14Data.b_SW1Pressed;
+}
+inline BOOL esos_uiF14_isSW1Released (void) {
+    return (!_st_esos_uiF14Data.b_SW1Pressed);
+}
+inline BOOL esos_uiF14_isSW1DoublePressed (void) {
+    BOOL temp = _st_esos_uiF14Data.b_SW1DoublePressed;
+    _st_esos_uiF14Data.b_SW1DoublePressed = FALSE;
+    return temp;
+}
+inline void esos_uiF14_setSW1DoublePressedPeriod (uint16_t period) {
+    _st_esos_uiF14Data.u16_SW1DoublePressedPeriod = period;
+    return;
+}
+inline uint16_t esos_uiF14_getSW1DoublePressedPeriod (void) {
+    return _st_esos_uiF14Data.u16_SW1DoublePressedPeriod;
+}
+
+//SW2
+inline BOOL esos_uiF14_isSW2Pressed (void) {
+    return _st_esos_uiF14Data.b_SW2Pressed;
+}
+inline BOOL esos_uiF14_isSW2Released (void) {
+    return (!_st_esos_uiF14Data.b_SW2Pressed);
+}
+inline BOOL esos_uiF14_isSW2DoublePressed (void) {
+    BOOL temp = _st_esos_uiF14Data.b_SW2DoublePressed;
+    _st_esos_uiF14Data.b_SW2DoublePressed = FALSE;
+    return temp;
+}
+inline void esos_uiF14_setSW2DoublePressedPeriod (uint16_t period) {
+    _st_esos_uiF14Data.u16_SW2DoublePressedPeriod = period;
+    return;
+}
+inline uint16_t esos_uiF14_getSW2DoublePressedPeriod (void) {
+    return _st_esos_uiF14Data.u16_SW2DoublePressedPeriod;
+}
+//SW3
+inline BOOL esos_uiF14_isSW3Pressed (void) {
+    return _st_esos_uiF14Data.b_SW3Pressed;
+}
+inline BOOL esos_uiF14_isSW3Released (void) {
+    return (!_st_esos_uiF14Data.b_SW3Pressed);
+}
+inline BOOL esos_uiF14_isSW3DoublePressed (void) {
+    BOOL temp = _st_esos_uiF14Data.b_SW3DoublePressed;
+    _st_esos_uiF14Data.b_SW3DoublePressed = FALSE;
+    return temp;
+}
+inline void esos_uiF14_setSW3DoublePressedPeriod (uint16_t period) {
+    _st_esos_uiF14Data.u16_SW3DoublePressedPeriod = period;
+    return;
+}
+inline uint16_t esos_uiF14_getSW3DoublePressedPeriod (void) {
+    return _st_esos_uiF14Data.u16_SW3DoublePressedPeriod;
+}
+
+
+    // PUBLIC LED FUNCTIONS
+
+//LED1
+inline BOOL esos_uiF14_isLED1On (void) {
+    return (_st_esos_uiF14Data.b_LED1On==TRUE);
+}
+inline BOOL esos_uiF14_isLED1Off (void) {
+    return (_st_esos_uiF14Data.b_LED1On==FALSE);
+}
+inline void esos_uiF14_turnLED1On (void) {
+    _st_esos_uiF14Data.u16_LED1FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED1On = TRUE;
+    return;
+}
+inline void esos_uiF14_turnLED1Off (void) {
+    _st_esos_uiF14Data.u16_LED1FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED1On = FALSE;
+    return;
+}
+inline void esos_uiF14_toggleLED1 (void) {
+    _st_esos_uiF14Data.b_LED1On ^= 1;
+    LED1 = !LED1;
+    return;
+}
+inline void esos_uiF14_flashLED1( uint16_t u16_period) {
+    _st_esos_uiF14Data.u16_LED1FlashPeriod = u16_period;
+    return;
+}
+//LED2
+inline BOOL esos_uiF14_isLED2On (void) {
+    return (_st_esos_uiF14Data.b_LED2On==TRUE);
+}
+inline BOOL esos_uiF14_isLED2Off (void) {
+    return (_st_esos_uiF14Data.b_LED2On==FALSE);
+}
+inline void esos_uiF14_turnLED2On (void) {
+    _st_esos_uiF14Data.u16_LED2FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED2On = TRUE;
+    return;
+}
+inline void esos_uiF14_turnLED2Off (void) {
+    _st_esos_uiF14Data.u16_LED2FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED2On = FALSE;
+    return;
+}
+inline void esos_uiF14_toggleLED2 (void) {
+    _st_esos_uiF14Data.b_LED2On ^= 1;
+    LED2 = !LED2;
+    return;
+}
+inline void esos_uiF14_flashLED2( uint16_t u16_period) {
+    _st_esos_uiF14Data.u16_LED2FlashPeriod = u16_period;
+    return;
+}
+//LED3
+inline BOOL esos_uiF14_isLED3On (void) {
+    return (_st_esos_uiF14Data.b_LED3On==TRUE);
+}
+inline BOOL esos_uiF14_isLED3Off (void) {
+    return (_st_esos_uiF14Data.b_LED3On==FALSE);
+}
+inline void esos_uiF14_turnLED3On (void) {
+    _st_esos_uiF14Data.u16_LED3FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED3On = TRUE;
+    return;
+}
+inline void esos_uiF14_turnLED3Off (void) {
+    _st_esos_uiF14Data.u16_LED3FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED3On = FALSE;
+    return;
+}
+inline void esos_uiF14_toggleLED3 (void) {
+    _st_esos_uiF14Data.b_LED3On ^= 1;
+    LED3 = !LED3;
+    return;
+}
+inline void esos_uiF14_flashLED3( uint16_t u16_period) {
+    _st_esos_uiF14Data.u16_LED3FlashPeriod = u16_period;
+    return;
+}
+
+    // PUBLIC RPG FUNCTIONS
+
+inline uint16_t esos_uiF14_getRPGValue_u16 ( void ) {
+    return _st_esos_uiF14Data.i16_RPGCounter;
+}
+inline BOOL esos_uiF14_isRPGTurning ( void ) {
+    return _st_esos_uiF14Data.b_RPGMoving;
+}
+inline BOOL esos_uiF14_isRPGTurningSlow( void ) {
+    return _st_esos_uiF14Data.b_RPGMoving_slow;
+}
+inline BOOL esos_uiF14_isRPGTurningMedium( void ) {
+    return _st_esos_uiF14Data.b_RPGMoving_med;
+}
+inline BOOL esos_uiF14_isRPGTurningFast( void ) {
+    return _st_esos_uiF14Data.b_RPGMoving_fast;
+}
+inline BOOL esos_uiF14_isRPGTurningCW( void ) {
+    return _st_esos_uiF14Data.b_RPGTurning_CW;
+}
+inline BOOL esos_uiF14_isRPGTurningCCW( void ) {
+    return _st_esos_uiF14Data.b_RPGTurning_CCW;
+}
+inline uint16_t esos_uiF14_getRPGPeriod_u16( void ) {
+    return _st_esos_uiF14Data.u16_RPGPeriod_ms;
+}
+inline void esos_uiF14_setRPG_noVelocity( uint16_t noVelocity ) {
+    _st_esos_uiF14Data.u16_noVelocity = noVelocity;
+    return;
+}
+inline uint16_t esos_uiF14_getRPGSlowThreshold(void) {
+    return _st_esos_uiF14Data.u16_noVelocity;
+}
+inline void esos_uiF14_setRPG_medVelocity( uint16_t medVelocity ) {
+    _st_esos_uiF14Data.u16_medVelocity = medVelocity;
+    return;
+}
+inline uint16_t esos_uiF14_getRPGMediumThreshold(void) {
+    return _st_esos_uiF14Data.u16_medVelocity;
+}
+inline void esos_uiF14_setRPG_fastVelocity( uint16_t fastVelocity ) {
+    _st_esos_uiF14Data.u16_fastVelocity = fastVelocity;
+    return;
+}
+inline uint16_t esos_uiF14_getRPGFastThreshold(void) {
+    return _st_esos_uiF14Data.u16_fastVelocity;
+}
+
+
+void init_defaults( void ) { // Set default values
+    _st_esos_uiF14Data.b_SW1Pressed = FALSE;
+    _st_esos_uiF14Data.b_SW1DoublePressed = FALSE;
+    _st_esos_uiF14Data.u16_SW1DoublePressedPeriod = 500;
+    _st_esos_uiF14Data.b_SW2Pressed = FALSE;
+    _st_esos_uiF14Data.b_SW2DoublePressed = FALSE;
+    _st_esos_uiF14Data.u16_SW2DoublePressedPeriod = 500;
+    _st_esos_uiF14Data.b_SW3Pressed = FALSE;
+    _st_esos_uiF14Data.b_SW3DoublePressed = FALSE;
+    _st_esos_uiF14Data.u16_SW3DoublePressedPeriod = 500;
+
+    _st_esos_uiF14Data.b_RPGALast = FALSE;
+    _st_esos_uiF14Data.b_RPGBLast = FALSE;
+    _st_esos_uiF14Data.b_RPGMoving = FALSE;
+    _st_esos_uiF14Data.b_RPGMoving_slow = FALSE;
+    _st_esos_uiF14Data.b_RPGMoving_med = FALSE;
+    _st_esos_uiF14Data.b_RPGMoving_fast = FALSE;
+    _st_esos_uiF14Data.b_RPGTurning_CW = FALSE;
+    _st_esos_uiF14Data.b_RPGTurning_CCW = FALSE;
+
+    _st_esos_uiF14Data.u16_noVelocity = 400;
+    _st_esos_uiF14Data.u16_medVelocity = 150;
+    _st_esos_uiF14Data.u16_fastVelocity = 15;
+
+    _st_esos_uiF14Data.b_LED1On = FALSE;
+    _st_esos_uiF14Data.u16_LED1FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED2On = FALSE;
+    _st_esos_uiF14Data.u16_LED2FlashPeriod = 0;
+    _st_esos_uiF14Data.b_LED3On = FALSE;
+    _st_esos_uiF14Data.u16_LED3FlashPeriod = 0;
+
+    _st_esos_uiF14Data.i16_RPGCounter = 0;
+    _st_esos_uiF14Data.i16_lastRPGCounter = 0;
+    _st_esos_uiF14Data.u16_RPGPeriod_ms = 0;
+    _st_esos_uiF14Data.u16_RPGLastPeriod_ms = 0;
+
+    return;
+}
+
+ESOS_USER_TASK( __esos_uiF14_task ) {
+
+    uint16_t time_since_change = 0;
+
+    ESOS_TASK_BEGIN();
+    while(TRUE) {
+
+        /* BEGIN RPG STUFF */
+        time_since_change = (esos_GetSystemTick()) - (_st_esos_uiF14Data.u16_RPGLastPeriod_ms);
+        // Check if RPGA has changed
+        if (RPGA != _st_esos_uiF14Data.b_RPGALast) {
+            // RPG is moving
+            _st_esos_uiF14Data.b_RPGMoving = TRUE;
+            // Set RPGALast to whatever RPGA is right now
+            _st_esos_uiF14Data.b_RPGALast = RPGA;
+
+            // Calculate time since last change in RPG. Used to calculate velocity
+            _st_esos_uiF14Data.u16_RPGPeriod_ms = (esos_GetSystemTick()) - (_st_esos_uiF14Data.u16_RPGLastPeriod_ms);
+
+            // New method for getting last period
+            _st_esos_uiF14Data.u16_RPGLastPeriod_ms = esos_GetSystemTick();
+
+            // Figure out what speed the RPG is moving (slow, medium, fast)
+            //Slow
+            if (_st_esos_uiF14Data.u16_RPGPeriod_ms >= _st_esos_uiF14Data.u16_medVelocity) {
+                _st_esos_uiF14Data.b_RPGMoving_slow = TRUE;
+                _st_esos_uiF14Data.b_RPGMoving_med = FALSE;
+                _st_esos_uiF14Data.b_RPGMoving_fast = FALSE;
+            }
+
+            // Medium
+            else if ((_st_esos_uiF14Data.u16_RPGPeriod_ms <= _st_esos_uiF14Data.u16_medVelocity) && (_st_esos_uiF14Data.u16_RPGPeriod_ms > _st_esos_uiF14Data.u16_fastVelocity)) {
+                _st_esos_uiF14Data.b_RPGMoving_slow = FALSE;
+                _st_esos_uiF14Data.b_RPGMoving_med = TRUE;
+                _st_esos_uiF14Data.b_RPGMoving_fast = FALSE;
+            }
+            // Fast
+            else {
+                _st_esos_uiF14Data.b_RPGMoving_slow = FALSE;
+                _st_esos_uiF14Data.b_RPGMoving_med = FALSE;
+                _st_esos_uiF14Data.b_RPGMoving_fast = TRUE;
+            }
+
+            // Figure out which direction the RPG is moving (clockwise or counter-clockwise)
+            // Clockwise
+            if ((RPGA == 1 && RPGB == 0) || (RPGA == 0 && RPGB == 1) ) {
+                _st_esos_uiF14Data.b_RPGTurning_CW = TRUE;
+                _st_esos_uiF14Data.b_RPGTurning_CCW = FALSE;
+                _st_esos_uiF14Data.i16_RPGCounter += 1;
+            }
+            // Counter-clockwise
+            else {
+                _st_esos_uiF14Data.b_RPGTurning_CW = FALSE;
+                _st_esos_uiF14Data.b_RPGTurning_CCW = TRUE;
+                _st_esos_uiF14Data.i16_RPGCounter -= 1;
+            }
+                            
+        }
+        // If the RPG is not moving, set all of the movement related variables to false
+        else if (time_since_change > _st_esos_uiF14Data.u16_noVelocity) {
+            _st_esos_uiF14Data.b_RPGMoving = FALSE;
+            _st_esos_uiF14Data.b_RPGMoving_slow = FALSE;
+            _st_esos_uiF14Data.b_RPGMoving_med = FALSE;
+            _st_esos_uiF14Data.b_RPGMoving_fast = FALSE;
+            _st_esos_uiF14Data.b_RPGTurning_CW = FALSE;
+            _st_esos_uiF14Data.b_RPGTurning_CCW = FALSE;
+        }
+        ESOS_TASK_WAIT_TICKS( __ESOS_UIF14_UI_PERIOD_MS ); // Wait 10 ms and run again
+    }
+    ESOS_TASK_END();
+}
+
+/* BEGIN LED STUFF */
+
+ESOS_USER_TASK(update_LED1) {
+    ESOS_TASK_BEGIN();
+    while(1) {
+        if(_st_esos_uiF14Data.u16_LED1FlashPeriod != 0) _st_esos_uiF14Data.b_LED1On ^= 1;
+        if(_st_esos_uiF14Data.b_LED1On == TRUE) LED1_ON();
+        else LED1_OFF();
+        if(_st_esos_uiF14Data.u16_LED1FlashPeriod != 0) ESOS_TASK_WAIT_TICKS(_st_esos_uiF14Data.u16_LED1FlashPeriod / 2);
+        ESOS_TASK_YIELD();
+    }
+    ESOS_TASK_END();
+}
+ESOS_USER_TASK(update_LED2) {
+    ESOS_TASK_BEGIN();
+    while(1) {
+        if(_st_esos_uiF14Data.u16_LED2FlashPeriod != 0) _st_esos_uiF14Data.b_LED2On ^= 1;
+        if(_st_esos_uiF14Data.b_LED2On == TRUE) LED2_ON();
+        else LED2_OFF();
+        if(_st_esos_uiF14Data.u16_LED2FlashPeriod != 0) ESOS_TASK_WAIT_TICKS(_st_esos_uiF14Data.u16_LED2FlashPeriod / 2);
+        ESOS_TASK_YIELD();
+    }
+    ESOS_TASK_END();
+}
+ESOS_USER_TASK(update_LED3) {
+    ESOS_TASK_BEGIN();
+    while(1) {
+        if(_st_esos_uiF14Data.u16_LED3FlashPeriod != 0) _st_esos_uiF14Data.b_LED3On ^= 1;
+        if(_st_esos_uiF14Data.b_LED3On == TRUE) LED3_ON();
+        else LED3_OFF();
+        if(_st_esos_uiF14Data.u16_LED3FlashPeriod != 0) ESOS_TASK_WAIT_TICKS(_st_esos_uiF14Data.u16_LED3FlashPeriod / 2);
+        ESOS_TASK_YIELD();
+    }
+    ESOS_TASK_END();
+}
+
+/* BEGIN SWITCH INTERRUPT STUFF */
+
+ESOS_USER_TIMER(SW_Update)
+{
+    //Check switches every 60ms and use their value at that moment as a debounced switch value
+    SW1_DEBOUNCED = SW1_PRESSED;
+    SW2_DEBOUNCED = SW2_PRESSED;
+    SW3_DEBOUNCED = SW3_PRESSED;
+    
+    _st_esos_uiF14Data.b_SW1Pressed = SW1_DEBOUNCED;
+    _st_esos_uiF14Data.b_SW2Pressed = SW2_DEBOUNCED;
+    _st_esos_uiF14Data.b_SW3Pressed = SW3_DEBOUNCED;
+    
+//    if(_st_esos_uiF14Data.b_SW1Pressed) esos_uiF14_turnLED1On();
+//    else esos_uiF14_turnLED1Off();
+//    if(_st_esos_uiF14Data.b_SW2Pressed) esos_uiF14_turnLED2On();
+//    else esos_uiF14_turnLED2Off();
+//    if(_st_esos_uiF14Data.b_SW3Pressed) esos_uiF14_turnLED3On();
+//    else esos_uiF14_turnLED3Off();
+
+    //Reset IC Buffers if only a single press occurred
+    if (SW1_IC_BUFFER / CYCLES_PER_MS >= _st_esos_uiF14Data.u16_SW1DoublePressedPeriod) SW_IC_BUFFER_RESET(11,12);
+    if (SW2_IC_BUFFER / CYCLES_PER_MS >= _st_esos_uiF14Data.u16_SW2DoublePressedPeriod) SW_IC_BUFFER_RESET(13,14);
+    if (SW3_IC_BUFFER / CYCLES_PER_MS >= _st_esos_uiF14Data.u16_SW3DoublePressedPeriod) SW_IC_BUFFER_RESET(15,16);
+}
+
+ESOS_USER_INTERRUPT(ESOS_IRQ_PIC24_IC11) //Interrupt for SW1 press
+{
+    BOOL debounce = !SW1_DEBOUNCED;
+    if (debounce) {
+        //_st_esos_uiF14Data.b_SW1Pressed = 1;
+        _st_esos_uiF14Data.b_SW1DoublePressed = 0;
+        if (IC11CON2bits.ICTRIG == 0) {
+            if(SW1_IC_BUFFER / CYCLES_PER_MS < _st_esos_uiF14Data.u16_SW1DoublePressedPeriod) {
+                _st_esos_uiF14Data.b_SW1DoublePressed = 1;
+//                esos_uiF14_turnLED1On();
+            }
+        }
+        else {
+            SW_IC_BUFFER_RESET(11,12);
+            IC11CON2bits.ICTRIG = IC12CON2bits.ICTRIG = 0; //resume IC
+        }
+    }
+    ESOS_MARK_PIC24_USER_INTERRUPT_SERVICED(ESOS_IRQ_PIC24_IC11);
+}
+ESOS_USER_INTERRUPT(ESOS_IRQ_PIC24_IC13) //Interrupt for SW2 press
+{
+    BOOL debounce = !SW2_DEBOUNCED;
+    if (debounce) {
+//        _st_esos_uiF14Data.b_SW2Pressed = 1;
+        _st_esos_uiF14Data.b_SW2DoublePressed = 0;
+        if (IC13CON2bits.ICTRIG == 0) {
+            if(SW2_IC_BUFFER / CYCLES_PER_MS < _st_esos_uiF14Data.u16_SW2DoublePressedPeriod) {
+                _st_esos_uiF14Data.b_SW2DoublePressed = 1;
+//                esos_uiF14_turnLED2On();
+            }
+        }
+        else {
+            SW_IC_BUFFER_RESET(13,14);
+            IC13CON2bits.ICTRIG = IC14CON2bits.ICTRIG = 0; //resume IC
+        }
+    }
+    ESOS_MARK_PIC24_USER_INTERRUPT_SERVICED(ESOS_IRQ_PIC24_IC13);
+}
+ESOS_USER_INTERRUPT(ESOS_IRQ_PIC24_IC15) //Interrupt for SW3 press
+{
+    BOOL debounce = !SW3_DEBOUNCED;
+    if (debounce) {
+//        _st_esos_uiF14Data.b_SW3Pressed = 1;
+        _st_esos_uiF14Data.b_SW3DoublePressed = 0;
+        if (IC15CON2bits.ICTRIG == 0) {
+            if(SW3_IC_BUFFER / CYCLES_PER_MS < _st_esos_uiF14Data.u16_SW3DoublePressedPeriod) {
+                _st_esos_uiF14Data.b_SW3DoublePressed = 1;
+//                esos_uiF14_turnLED3On();
+            }
+        }
+        else {
+            SW_IC_BUFFER_RESET(15,16);
+            IC15CON2bits.ICTRIG = IC16CON2bits.ICTRIG = 0; //resume IC
+        }
+    }
+    ESOS_MARK_PIC24_USER_INTERRUPT_SERVICED(ESOS_IRQ_PIC24_IC15);
+}
+// UIF14 INITIALIZATION FUNCTION
+
+void config_esos_uiF14() {
+    __esos_unsafe_PutString(HELLO_MSG);
+    init_defaults();
+    
+    CONFIG_LED1();
+    CONFIG_LED2();
+    CONFIG_LED3();
+    CONFIG_SW1();
+    CONFIG_SW2();
+    CONFIG_SW3();
+    CONFIG_SW1_DEBOUNCE_AND_IC();
+    CONFIG_SW2_DEBOUNCE_AND_IC();
+    CONFIG_SW3_DEBOUNCE_AND_IC();
+    CONFIG_RPG();
+    CONFIG_POT();
+    CONFIG_THERM();
+    
+    esos_RegisterTask(__esos_uiF14_task);
+    
+    esos_RegisterTimer(SW_Update, 60);
+
+    // These tasks did not function properly for me. Moved the code into the __esos_uiF14_task
+    esos_RegisterTask(update_LED1);
+    esos_RegisterTask(update_LED2);
+    esos_RegisterTask(update_LED3);
+    return;
+}
---
+++ Lab5/esos/src/esos_pic24_sensor.c
@@ -0,0 +1,103 @@
+#include "esos_pic24_sensor.h"
+
+
+//Configure and enable the sensor module for hwxxx hardware.
+// e_senCh   specifies sensor channel
+// e_senVRef specifies sensor voltage reference
+void esos_sensor_config_hw (esos_sensor_ch_t e_senCh, esos_sensor_vref_t e_senVRef) {
+//    CONFIG_ADC();
+    AD1CON1bits.ADON    = 0b0;    //Turn ADC off
+    AD1CON1bits.ADSIDL  = 0b0;    //Continue module operation in Idle mode
+    AD1CON1bits.FORM    = 0b0;    //Integer 16-bit (DOUT = 0000 0000 0000 0000 0000 00dd dddd dddd)
+    AD1CON1bits.SSRC    = 0b111;  //Internal counter ends sampling and starts conversion (auto convert)
+    AD1CON1bits.ASAM    = 0b0;    //Sampling begins when SAMP bit is set (change this to auto-sample)
+    AD1CON1bits.SAMP    = 0b0;    //The ADC sample/hold amplifier is holding
+    AD1CON1bits.DONE    = 0b0;    //Analog-to-digital conversion is not done or has not started
+    AD1CON1bits.ADDMABM = 0b1;    //DMA Buffers written in order of conversion
+    AD1CON1bits.AD12B   = 0b1;    //Enable 12 bit mode
+    AD1CON1bits.SSRCG   = 0b0;    //Sample clock source group
+    
+    AD1CON2bits.VCFG    = 0b001;  //Set ADC VR+ to VREF+ pin and VR- to AVSS pin
+    AD1CON2bits.CSCNA   = 0b0;    //Do not scan inputs
+    AD1CON2bits.SMPI    = 0b0000; //Interrupts at the completion of conversion for each sample/convert sequence
+    AD1CON2bits.BUFM    = 0b0;    //Buffer configured as one 16-word buffer ADC1BUF(15...0.)
+    AD1CON2bits.ALTS    = 0b0;    //Always use MUX A input multiplexer settings
+    AD1CON2bits.CHPS    = 0b00;   //Channel select bits
+    
+    AD1CON3bits.ADRC    = 0b1;    //ADC internal RC clock
+    AD1CON3bits.SAMC    = 0b11111;//Auto-sample Time bits: 31 TAD
+    AD1CON4bits.ADDMAEN = 0b0;
+
+    AD1CHS0bits.CH0SA = e_senCh;  //Set CH0+ input AN[e_senCh]
+    AD1CHS0bits.CH0NA = 0;        //Set CHO- input to VREFL (AVss)
+    AD1CHS123 = 0;                //Disable CH's 1,2,&3
+    AD1CSSH = 0x0000;             //No inputs are scanned
+    AD1CSSL = 0x0000;             //Set input scan select register low
+//    IFS1CLR = 2; //Clear ADC conversion interrupt
+    AD1CON1bits.ADON = 1;         //Turn ADC on
+}
+
+//Determine if the sensor is currently converting
+BOOL esos_sensor_is_converting_hw (void) {
+    if (AD1CON1bits.DONE == 1) {
+        esos_ClearUserFlag(ESOS_SENSOR_IS_CONVERTING);
+        return FALSE;
+    }
+    return esos_IsUserFlagSet(ESOS_SENSOR_IS_CONVERTING);
+}
+
+//Initiate a conversion for a configured sensor
+void esos_sensor_initiate_hw (void) {
+    esos_SetUserFlag(__ESOS_SYS_ADC_IS_BUSY);
+    AD1CON1bits.SAMP = 1;
+}
+
+//Receive the value from a conversion that has already been initiated
+uint16_t esos_sensor_getvalue_u16_hw (void) {
+    if (!esos_IsUserFlagClear(ESOS_SENSOR_IS_CONVERTING)) return 0;
+    return ADC1BUF0;
+}
+
+//Release any pending conversions for the sensor
+void esos_sensor_release_hw (void) {
+    esos_ClearUserFlag(ESOS_SENSOR_IS_CONVERTING);
+    AD1CON1bits.ADON = 0; // turn off the adc
+}
+
+
+int uint32_to_str(uint32_t u32_val, char *str, uint32_t len)
+{
+    // Catches out any time these two values are set to 0
+    if (len == 0 || u32_val == 0)
+        return -1;
+
+    //printf("\nTEMP_INT: %lu\n", u32_val);
+    uint32_t digit;
+    uint16_t i = 0;
+    // Stores all of the digits from u32_val into the string BACKWARDS
+    while (u32_val && (i < (len - 1))) {
+        
+        digit = u32_val % 10; // Split off the last digit of the number
+        str[i] = '0' + digit; // Store the digit in the string
+        u32_val /= 10; // Divide off the digit that was just stored in the string
+        i++; // Increment
+        
+    }
+    str[i] = '\0'; // End of string
+    
+    // String reversal
+    // The string reversal is really weird because we're dealing with char *
+    char *temp = str;
+    char *also_temp = temp;
+    while (also_temp && *also_temp)
+        ++also_temp;
+    for (--also_temp; temp < also_temp; ++temp, --also_temp) {
+        // Lots of XORs
+        *temp = *temp ^ *also_temp; 
+        *also_temp = *temp ^ *also_temp; 
+        *temp = *temp ^ *also_temp;
+    }
+
+   return 0;
+}
+
