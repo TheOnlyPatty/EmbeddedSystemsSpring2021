cbd92cc94e0e3645b4af01f9438c2cd27d64652e
Joshua McCown <jpm623@msstate.edu>
2021-02-25 21:18:49 -0600

    finished t4_sensor2.c

--- Lab4/build/source_files/t4_sensor2.c
+++ Lab4/build/source_files/t4_sensor2.c
@@ -6,111 +6,184 @@
 #include "revF14.h"
 #include <stdio.h>
 
-//NOT FINISHED
+static char str_INSTRUCTIONS1[] = "Press SW1 to sample the potentiometer once.\nPress SW2 to begin sampling the potentiometer once per second.\nPress SW3 for further options.\n\n";
+static char str_INSTRUCTIONS2[] = "Press SW1, SW2, or SW3 to quit sampling.\n";
+static char str_ONE_SHOT_POT_PRE[] = "Potentiometer reading: ";
 
-static esos_sensor_process_t process_mode;
+static esos_sensor_process_t processing_mode = ESOS_SENSOR_ONE_SHOT;
 
-//Child task to print menu and set processing mode for main user tasks 
-ESOS_CHILD_TASK(mode_selection) {
-    static uint8_t mode;
-    static uint8_t sample_size;
+static enum STATE {
+    INSTRUCT,
+    WAIT,
+    ONCE,
+    CONTINUOUS,
+    MENU,
+} MENU_STATE;
 
+ESOS_USER_TASK(StateChange) {
+    static BOOL SW1_STATE, SW2_STATE, SW3_STATE;
     ESOS_TASK_BEGIN();
-	
-	
-    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();     
-    ESOS_TASK_WAIT_ON_SEND_STRING("Mode Selection: \n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("1. one-shot\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("2. average\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("3. minimum\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("4. maximum\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("5. median\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("**********************\n");
-    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-
-    ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM(); 
-    ESOS_TASK_WAIT_ON_GET_UINT8(mode); 
-    ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
-
-    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
-	//sample size shouldn't matter for one-shot, right? 
-    ESOS_TASK_WAIT_ON_SEND_STRING("Sample Size Selection (doesn't matter for one-shot): \n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("1. two");
-    ESOS_TASK_WAIT_ON_SEND_STRING("2. four\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("3. eight\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("4. sixteen\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("5. thirty-two\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("6. sixty-four\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("**********************\n");
-    ESOS_TASK_WAIT_ON_SEND_STRING("> ");
-    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-
-    ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();
-    ESOS_TASK_WAIT_ON_GET_UINT8(sample_size);
-    ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
-
-	//sets processing_mode based on input
-	//one-shot
-    if(mode == '1'){process_mode = ESOS_SENSOR_ONE_SHOT;}
-	//avg
-	else if(mode == '2' && sample_size == '1'){process_mode = ESOS_SENSOR_AVG2;}
-	else if(mode == '2' && sample_size == '2'){process_mode = ESOS_SENSOR_AVG4;}
-	else if(mode == '2' && sample_size == '3'){process_mode = ESOS_SENSOR_AVG8;}
-	else if(mode == '2' && sample_size == '4'){process_mode = ESOS_SENSOR_AVG16;}
-	else if(mode == '2' && sample_size == '5'){process_mode = ESOS_SENSOR_AVG32;}
-	else if(mode == '2' && sample_size == '6'){process_mode = ESOS_SENSOR_AVG64;}
-	//min
-	else if(mode == '3' && sample_size == '1'){process_mode = ESOS_SENSOR_MIN2;}
-	else if(mode == '3' && sample_size == '2'){process_mode = ESOS_SENSOR_MIN4;}
-	else if(mode == '3' && sample_size == '3'){process_mode = ESOS_SENSOR_MIN8;}
-	else if(mode == '3' && sample_size == '4'){process_mode = ESOS_SENSOR_MIN16;}
-	else if(mode == '3' && sample_size == '5'){process_mode = ESOS_SENSOR_MIN32;}
-	else if(mode == '3' && sample_size == '6'){process_mode = ESOS_SENSOR_MIN64;}
-	//max
-	else if(mode == '4' && sample_size == '1'){process_mode = ESOS_SENSOR_MAX2;}
-	else if(mode == '4' && sample_size == '2'){process_mode = ESOS_SENSOR_MAX4;}
-	else if(mode == '4' && sample_size == '3'){process_mode = ESOS_SENSOR_MAX8;}
-	else if(mode == '4' && sample_size == '4'){process_mode = ESOS_SENSOR_MAX16;}
-	else if(mode == '4' && sample_size == '5'){process_mode = ESOS_SENSOR_MAX32;}
-	else if(mode == '4' && sample_size == '6'){process_mode = ESOS_SENSOR_MAX64;}
-	//median
-	else if(mode == '5' && sample_size == '1'){process_mode = ESOS_SENSOR_MEDIAN2;}
-	else if(mode == '5' && sample_size == '2'){process_mode = ESOS_SENSOR_MEDIAN4;}
-	else if(mode == '5' && sample_size == '3'){process_mode = ESOS_SENSOR_MEDIAN8;}
-	else if(mode == '5' && sample_size == '4'){process_mode = ESOS_SENSOR_MEDIAN16;}
-	else if(mode == '5' && sample_size == '5'){process_mode = ESOS_SENSOR_MEDIAN32;}
-	else if(mode == '5' && sample_size == '6'){process_mode = ESOS_SENSOR_MEDIAN64;}
-	
-	//invalid selection was entered, what should we default process_mode to in this case? 
-	else {process_mode = ESOS_SENSOR_ONE_SHOT;}
-
-    ESOS_TASK_END();
-}
-
-ESOS_USER_TASK(sensor) {
-	//child task memory allocation
-    static ESOS_TASK_HANDLE child; 
-    ESOS_TASK_BEGIN();
-	while(TRUE){
-		
-		
-		//TODO: implement SW1 and SW2 presses, and the sensor stuff
-		
-		
-		//if switch 3 is pressed, run mode_selection child tasks which sets process_mode
-		if (esos_uiF14_isSW3Pressed()){ 
-			ESOS_ALLOCATE_CHILD_TASK(child);
-			ESOS_TASK_SPAWN_AND_WAIT(child, mode_selection);
-		}
-
+    while (1) {
+        if (SW1_STATE != esos_uiF14_isSW1Pressed()) { //Require a release before changing state again
+            SW1_STATE = esos_uiF14_isSW1Pressed();
+            if (SW1_STATE) {
+                if (MENU_STATE == CONTINUOUS) 
+                    MENU_STATE = INSTRUCT;
+				
+                else 
+                    MENU_STATE = ONCE;
+                
+            }
+        }
+        if (SW2_STATE != esos_uiF14_isSW2Pressed()) { //Require a release before changing state again
+            SW2_STATE = esos_uiF14_isSW2Pressed();
+            if (SW2_STATE) {
+                if (MENU_STATE == CONTINUOUS) 
+                    MENU_STATE = INSTRUCT;
+				
+                else
+                    MENU_STATE = CONTINUOUS;
+				
+            }
+        }
+        if (SW3_STATE != esos_uiF14_isSW3Pressed()) {
+            SW3_STATE = esos_uiF14_isSW3Pressed();
+            if (SW3_STATE) {
+                if (MENU_STATE == CONTINUOUS)
+                    MENU_STATE = INSTRUCT;
+				
+                else
+                    MENU_STATE = MENU;
+				
+            }
+        }
+            
+        ESOS_TASK_YIELD();
     }
-
-      
     ESOS_TASK_END();
 }
 
-void user_init(void){
+static uint16_t u16_data;
+
+ESOS_USER_TASK(SerialOutput) {
+    ESOS_TASK_BEGIN();
+    while (1) {
+        if(MENU_STATE == INSTRUCT) {
+            ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+            ESOS_TASK_WAIT_ON_SEND_STRING(str_INSTRUCTIONS1);
+            ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+            MENU_STATE = WAIT;
+        }
+        else if(MENU_STATE == WAIT) {
+
+        }
+        else if(MENU_STATE == ONCE) {
+            ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(POT_CHANNEL, ESOS_SENSOR_VREF_3V3);
+            ESOS_TASK_WAIT_SENSOR_READ(u16_data, processing_mode, ESOS_SENSOR_FORMAT_BITS);
+            ESOS_SENSOR_CLOSE();
+            ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+            ESOS_TASK_WAIT_ON_SEND_STRING(str_ONE_SHOT_POT_PRE);
+            ESOS_TASK_WAIT_ON_SEND_UINT16_AS_HEX_STRING(u16_data);
+            ESOS_TASK_WAIT_ON_SEND_UINT8('\n');
+            ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+            MENU_STATE = INSTRUCT;
+        }
+        else if(MENU_STATE == CONTINUOUS) {
+            ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+            ESOS_TASK_WAIT_ON_SEND_STRING(str_INSTRUCTIONS2);
+            ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+            ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(POT_CHANNEL, ESOS_SENSOR_VREF_3V3);
+            while(MENU_STATE == CONTINUOUS) {
+                ESOS_TASK_WAIT_SENSOR_READ(u16_data, processing_mode, ESOS_SENSOR_FORMAT_BITS);
+                ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+                ESOS_TASK_WAIT_ON_SEND_STRING(str_ONE_SHOT_POT_PRE);
+                ESOS_TASK_WAIT_ON_SEND_UINT16_AS_HEX_STRING(u16_data);
+                ESOS_TASK_WAIT_ON_SEND_UINT8('\n');
+                ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+                ESOS_TASK_WAIT_TICKS(1000);
+            }
+		}
+		else if(MENU_STATE == MENU) {
+            static uint8_t mode;
+			static uint8_t sample_size;
+			
+			ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();     
+			ESOS_TASK_WAIT_ON_SEND_STRING("Mode Selection: \n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("1. one-shot\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("2. average\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("3. minimum\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("4. maximum\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("5. median\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("**********************\n");
+			ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+
+			ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM(); 
+			ESOS_TASK_WAIT_ON_GET_UINT8(mode); 
+			ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
+
+			ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+			//sample size shouldn't matter for one-shot, right? 
+			ESOS_TASK_WAIT_ON_SEND_STRING("Sample Size Selection (doesn't matter for one-shot): \n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("1. two");
+			ESOS_TASK_WAIT_ON_SEND_STRING("2. four\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("3. eight\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("4. sixteen\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("5. thirty-two\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("6. sixty-four\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("**********************\n");
+			ESOS_TASK_WAIT_ON_SEND_STRING("> ");
+			ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+
+			ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();
+			ESOS_TASK_WAIT_ON_GET_UINT8(sample_size);
+			ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
+
+			//sets processing_mode based on input
+			//one-shot
+			if(mode == '1'){processing_mode = ESOS_SENSOR_ONE_SHOT;}
+			//avg
+			else if(mode == '2' && sample_size == '1'){processing_mode = ESOS_SENSOR_AVG2;}
+			else if(mode == '2' && sample_size == '2'){processing_mode = ESOS_SENSOR_AVG4;}
+			else if(mode == '2' && sample_size == '3'){processing_mode = ESOS_SENSOR_AVG8;}
+			else if(mode == '2' && sample_size == '4'){processing_mode = ESOS_SENSOR_AVG16;}
+			else if(mode == '2' && sample_size == '5'){processing_mode = ESOS_SENSOR_AVG32;}
+			else if(mode == '2' && sample_size == '6'){processing_mode = ESOS_SENSOR_AVG64;}
+			//min
+			else if(mode == '3' && sample_size == '1'){processing_mode = ESOS_SENSOR_MIN2;}
+			else if(mode == '3' && sample_size == '2'){processing_mode = ESOS_SENSOR_MIN4;}
+			else if(mode == '3' && sample_size == '3'){processing_mode = ESOS_SENSOR_MIN8;}
+			else if(mode == '3' && sample_size == '4'){processing_mode = ESOS_SENSOR_MIN16;}
+			else if(mode == '3' && sample_size == '5'){processing_mode = ESOS_SENSOR_MIN32;}
+			else if(mode == '3' && sample_size == '6'){processing_mode = ESOS_SENSOR_MIN64;}
+			//max
+			else if(mode == '4' && sample_size == '1'){processing_mode = ESOS_SENSOR_MAX2;}
+			else if(mode == '4' && sample_size == '2'){processing_mode = ESOS_SENSOR_MAX4;}
+			else if(mode == '4' && sample_size == '3'){processing_mode = ESOS_SENSOR_MAX8;}
+			else if(mode == '4' && sample_size == '4'){processing_mode = ESOS_SENSOR_MAX16;}
+			else if(mode == '4' && sample_size == '5'){processing_mode = ESOS_SENSOR_MAX32;}
+			else if(mode == '4' && sample_size == '6'){processing_mode = ESOS_SENSOR_MAX64;}
+			//median
+			else if(mode == '5' && sample_size == '1'){processing_mode = ESOS_SENSOR_MEDIAN2;}
+			else if(mode == '5' && sample_size == '2'){processing_mode = ESOS_SENSOR_MEDIAN4;}
+			else if(mode == '5' && sample_size == '3'){processing_mode = ESOS_SENSOR_MEDIAN8;}
+			else if(mode == '5' && sample_size == '4'){processing_mode = ESOS_SENSOR_MEDIAN16;}
+			else if(mode == '5' && sample_size == '5'){processing_mode = ESOS_SENSOR_MEDIAN32;}
+			else if(mode == '5' && sample_size == '6'){processing_mode = ESOS_SENSOR_MEDIAN64;}
+			
+			//invalid selection was entered, what should we default processing_mode to in this case? 
+			else {processing_mode = ESOS_SENSOR_ONE_SHOT;}
+		}
+		ESOS_TASK_YIELD();
+	}
+	ESOS_TASK_END();
+}
+
+void user_init(void) {
     config_esos_uiF14();
-	esos_uiF14_flashLED3(250);
-    esos_RegisterTask(sensor);
+
+    esos_uiF14_flashLED3(500); //500 ms flash on LED3. (Heartbeat)
+    MENU_STATE = INSTRUCT;
+    esos_RegisterTask(StateChange);
+    esos_RegisterTask(SerialOutput);
+
 }
